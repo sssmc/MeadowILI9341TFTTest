023c4d84a55effa31ab56765ffd60f6f640aa9b4
Sebastien Robitaille <sebastien@robitaille.info>
2023-11-07 22:56:06 -0800

    Add project files.

---
+++ MeadowApp.cs
@@ -0,0 +1,72 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Displays;
+using Meadow.Peripherals.Leds;
+using System;
+using System.Threading.Tasks;
+
+namespace MeadowTFTTest
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        RgbPwmLed onboardLed;
+
+        MicroGraphics graphics;
+        Ili9341 display;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            onboardLed = new RgbPwmLed(
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                CommonType.CommonAnode);
+
+            Resolver.Log.Info("Create display driver instance");
+
+            display = new Ili9341
+            (
+                spiBus: Device.CreateSpiBus(),
+                chipSelectPin: Device.Pins.D13,
+                dcPin: Device.Pins.D14,
+                resetPin: Device.Pins.D15,
+                width: 240, height: 320
+            );
+
+            graphics = new MicroGraphics(display)
+            {
+                IgnoreOutOfBoundsPixels = true,
+                CurrentFont = new Font12x16()
+            };
+
+            return base.Initialize();
+        }
+
+        public override Task Run()
+        {
+            Resolver.Log.Info("Run...");
+
+            graphics.Clear();
+
+            graphics.Rotation = RotationType._270Degrees;
+
+            graphics.DrawTriangle(10, 30, 50, 50, 10, 50, Color.Red);
+            graphics.DrawRectangle(20, 45, 40, 20, Color.Yellow, false);
+            graphics.DrawCircle(50, 50, 40, Color.Blue, false);
+            graphics.DrawText(5, 5, "Meadow F7", Color.White);
+
+            graphics.Show();
+
+            //DisplayTest();
+
+            return base.Run();
+        }
+    }
+}
